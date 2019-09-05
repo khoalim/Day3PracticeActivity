01118e4df25dc253b112a4eccde9ec2acce98b36
Kim Hoalim <kthoalim@gmail.com>
2019-09-05 12:31:30 -0400

    Initial Commit

---
+++ Day3PracticeActivity/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day3PracticeActivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks the user for their name and if it equals "Jen", returns "That's my name, too!"
+            // Otherwise, return "Hi."
+
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+               
+        }
+    }
+}
