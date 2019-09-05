37aa0cecfc0c468b1f36c1eeb612bbec8487327f
Kim Hoalim <kthoalim@gmail.com>
2019-09-05 12:46:52 -0400

    Added Conditional

--- Day3PracticeActivity/Program.cs
+++ Day3PracticeActivity/Program.cs
@@ -9,8 +9,30 @@ namespace Day3PracticeActivity
             // Write a program that asks the user for their name and if it equals "Jen", returns "That's my name, too!"
             // Otherwise, return "Hi."
 
-            Console.WriteLine("What's your name?");
-            string name = Console.ReadLine();
+            Console.WriteLine("What's your name?"); // Ask user for their name
+
+            string name = Console.ReadLine().ToLower(); // Allow user to enter name and store name in variable
+
+            // Is name equal to "Jen"?
+
+            bool isJen = name.Equals("jen");
+
+            if (isJen)
+            {
+                Console.WriteLine("That's my name too!");
+            }
+
+            else
+            {
+                Console.WriteLine("Hi!");
+            }
+            
+
+         
+
+
+
+
                
         }
     }
